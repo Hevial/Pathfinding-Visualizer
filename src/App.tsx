48855b2edae2ebdc3fa8aaee2b0ad31029e80a92
Hevial <onicar04@gmail.com>
2024-11-17 12:04:12 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<h1 className="flex justify-center items-center text-3xl font-bold underline h-screen w-screen bg-slate-800">
+			Hello world!
+		</h1>
+	);
+}
+
+export default App;
